6f2f4b22578958ad1d767da6fc04c5f143cc46e7
Anderson Dias <andersondaraujo@gmail.com>
2017-06-14 09:33:28 -0300

    Fixing the double quotes error and creating the first spec with error
    I've replaced the single quotes with double quotes and now we get an error related to the cell.

--- spec/conway_game_of_life_spec.cr
+++ spec/conway_game_of_life_spec.cr
@@ -2,8 +2,8 @@ require "./spec_helper"
 
 describe ConwayGameOfLife do
   context "respects first law: any live cell with fewer than two live neighbours dies, as if caused by under-population" do
-    context 'cell with no live neighbours' do
-      it 'should be dead' do
+    context "cell with no live neighbours" do
+      it "should be dead" do
         expect(cell).to be_dead
       end
     end
