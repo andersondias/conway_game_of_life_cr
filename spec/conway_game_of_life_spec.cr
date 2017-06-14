6c7687b53e4fa99f0ea1f8ee03926f8d12c03d14
Anderson Dias <andersondaraujo@gmail.com>
2017-06-14 09:32:07 -0300

    First mistake: strings are wrapped in double quotes
    ```bash
    crystal spec
    Error in line 1: while requiring "./spec/conway_game_of_life_spec.cr"
    
    Syntax error in ./spec/conway_game_of_life_spec.cr:5: unterminated char literal, use double quotes for strings
    
        context 'cell with no live neighbours' do
    ```

--- spec/conway_game_of_life_spec.cr
+++ spec/conway_game_of_life_spec.cr
@@ -1,9 +1,11 @@
 require "./spec_helper"
 
 describe ConwayGameOfLife do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  context "respects first law: any live cell with fewer than two live neighbours dies, as if caused by under-population" do
+    context 'cell with no live neighbours' do
+      it 'should be dead' do
+        expect(cell).to be_dead
+      end
+    end
   end
 end
