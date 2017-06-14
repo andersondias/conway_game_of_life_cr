ecd62839f05bdc710bad5b8dab217beb5b4ea444
Anderson Dias <andersondaraujo@gmail.com>
2017-06-14 11:22:54 -0300

    Trying to improve spec by adding the concept of next generation
    Cells lives inside a given population and a population must evolve into new generations.
    In order to move to a new generation we must use population.next_generation and we should add cells to it.
    
    A cell must to be placed into a specific point of the population, described by the x and y position.
    
    In order to test it I've added a before_each macro to create the population in the beggining of each spec, but the
     following error was raised:
    
    ```
    Error in line 2: while requiring "./spec/conway_game_of_life_spec.cr"
    
    in spec/conway_game_of_life_spec.cr:5: undefined local variable or method 'before_each'
    
        before_each do
        ^~~~~~~~~~~
    ```
    
    It happens that I was used to rspec's callback syntax and it does not works in default spec suite for crystal.

--- spec/conway_game_of_life_spec.cr
+++ spec/conway_game_of_life_spec.cr
@@ -2,20 +2,41 @@ require "./spec_helper"
 
 describe ConwayGameOfLife do
   context "respects first law: any live cell with fewer than two live neighbours dies, as if caused by under-population" do
+    before_each do
+      @population = Population.new
+    end
+
     context "cell with no live neighbours" do
-      it "should be dead" do
+      it "should be dead in the next generation" do
         cell = ConwayGameOfLife::Cell.new
+        cell.alive!
+
+        cell.dead?.should be_falsey
+
+        @population.add_cell(cell, x: 0, y: 0)
+        @population.next_generation!
+
         cell.dead?.should be_truthy
       end
     end
 
     context "cell with one live neighbour" do
-      it "should be dead" do
+      it "should be dead in the next generation" do
         cell = ConwayGameOfLife::Cell.new
-        neighbour_cell = ConwayGameOfLife::Cell.new
+        cell.alive!
+
+        another_cell = ConwayGameOfLife::Cell.new
+        another_cell.alive!
+
+        cell.dead?.should be_falsey
+        another_cell.dead?.should be_falsey
+
+        @population.add_cell(cell, x: 0, y: 0)
+        @population.add_cell(another_cell, x: 1, y: 0)
+        @population.next_generation!
 
         cell.dead?.should be_truthy
-        neighbour_cell.dead?.should be_truthy
+        another_cell.dead?.should be_truthy
       end
     end
   end
