ceb4ef96ffa8ff90695ee8223cfba15830f18ba3
Anderson Dias <andersondaraujo@gmail.com>
2017-06-15 10:57:10 -0300

    Crystal arrays are not declared as in Ruby
    I'm trying to declare an array of cells in population class but I get this error:
    
    ```
    Syntax error in src/conway_game_of_life/population.cr:3: for empty arrays use '[] of ElementType'
    
        @cells = []
    ```
    
    This happens because arrays should be typed in Crystal.

--- src/conway_game_of_life/population.cr
+++ src/conway_game_of_life/population.cr
@@ -1,5 +1,10 @@
 class ConwayGameOfLife::Population
+  def initialize
+    @cells = []
+  end
+
   def add_cell(@cell : ConwayGameOfLife::Cell, x, y)
+    @cells << @cell
   end
 
   def next_generation!
