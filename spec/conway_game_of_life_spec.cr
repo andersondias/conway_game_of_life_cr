d0edb2ff27403251390c3638fbec2f8e4213049f
Anderson Dias <andersondaraujo@gmail.com>
2017-06-15 19:44:39 -0300

    Adding more scenarios to cover second law.
    Now we cover all possibilities involving bottom, top, left and right neighbours.

--- spec/conway_game_of_life_spec.cr
+++ spec/conway_game_of_life_spec.cr
@@ -75,5 +75,90 @@ describe ConwayGameOfLife do
         cell_at_botton.dead?.should be_true
       end
     end
+
+    context "cell with two live neighbours at left and top sides" do
+      it "should be alive in the next generation" do
+        population = ConwayGameOfLife::Population.new(length: 5, width: 5)
+
+        cell = population.find_cell(x: 2, y: 2)
+        cell.alive!
+
+        cell_at_top = population.find_cell(x: 2, y: 1)
+        cell_at_top.alive!
+
+        cell_at_left = population.find_cell(x: 1, y: 2)
+        cell_at_left.alive!
+
+        population.next_generation!
+
+        cell.alive?.should be_true
+        cell_at_top.dead?.should be_true
+        cell_at_left.dead?.should be_true
+      end
+    end
+
+    context "cell with two live neighbours at right and top sides" do
+      it "should be alive in the next generation" do
+        population = ConwayGameOfLife::Population.new(length: 5, width: 5)
+
+        cell = population.find_cell(x: 2, y: 2)
+        cell.alive!
+
+        cell_at_top = population.find_cell(x: 2, y: 1)
+        cell_at_top.alive!
+
+        cell_at_right = population.find_cell(x: 3, y: 2)
+        cell_at_right.alive!
+
+        population.next_generation!
+
+        cell.alive?.should be_true
+        cell_at_top.dead?.should be_true
+        cell_at_right.dead?.should be_true
+      end
+    end
+
+
+    context "cell with two live neighbours at left and bottom sides" do
+      it "should be alive in the next generation" do
+        population = ConwayGameOfLife::Population.new(length: 5, width: 5)
+
+        cell = population.find_cell(x: 2, y: 2)
+        cell.alive!
+
+        cell_at_bottom = population.find_cell(x: 2, y: 3)
+        cell_at_bottom.alive!
+
+        cell_at_left = population.find_cell(x: 1, y: 2)
+        cell_at_left.alive!
+
+        population.next_generation!
+
+        cell.alive?.should be_true
+        cell_at_bottom.dead?.should be_true
+        cell_at_left.dead?.should be_true
+      end
+    end
+
+    context "cell with two live neighbours at right and bottom sides" do
+      it "should be alive in the next generation" do
+        population = ConwayGameOfLife::Population.new(length: 5, width: 5)
+
+        cell = population.find_cell(x: 2, y: 2)
+        cell.alive!
+
+        cell_at_bottom = population.find_cell(x: 2, y: 3)
+        cell_at_bottom.alive!
+
+        cell_at_right = population.find_cell(x: 3, y: 2)
+        cell_at_right.alive!
+
+        population.next_generation!
+
+        cell.alive?.should be_true
+        cell_at_bottom.dead?.should be_true
+        cell_at_right.dead?.should be_true
+      end
+    end
   end
 end
