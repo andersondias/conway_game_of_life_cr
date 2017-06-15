89b775080573c844a54d357efa8e70282d6b2c80
Anderson Dias <andersondaraujo@gmail.com>
2017-06-15 19:39:41 -0300

    Improving second law specs with more scenarios
    The first spec to the second law was looking for cells at left and right sides alone, but there are a long number of possibilities where a cell can be surrounded by other 2 alive cells.
    
    In this spec we add cells at top and bottom of the given cell.

--- src/conway_game_of_life/population.cr
+++ src/conway_game_of_life/population.cr
@@ -1,9 +1,10 @@
 class ConwayGameOfLife::Population
-  def initialize(width, length)
+  def initialize(@width : Int32, @length : Int32)
     @cells = Array(Array(ConwayGameOfLife::Cell)).new
-    width.times do |x|
+
+    @width.times do |x|
       @cells << Array(ConwayGameOfLife::Cell).new
-      length.times do
+      @length.times do
         @cells[x] << ConwayGameOfLife::Cell.new(alive: false)
       end
     end
@@ -22,6 +23,37 @@ class ConwayGameOfLife::Population
   end
 
   def next_generation!
-    cells.each { |cell| cell.dead! }
+    about_to_die = Array(ConwayGameOfLife::Cell).new
+
+    @width.times do |x|
+      @length.times do |y|
+        current_cell = @cells[x][y]
+        next if current_cell.dead?
+
+        cell_to_the_left = @cells[x-1][y]
+        cell_to_the_right = @cells[x+1][y]
+
+        if y > 0
+          cell_to_the_top = @cells[x][y-1]
+        end
+
+        if y + 1 < @length
+          cell_to_the_botton = @cells[x][y+1]
+        end
+
+        alive_cells_around_current_cell = 0
+
+        alive_cells_around_current_cell += 1 if cell_to_the_left.alive?
+        alive_cells_around_current_cell += 1 if cell_to_the_right.alive?
+        alive_cells_around_current_cell += 1 if cell_to_the_top && cell_to_the_top.alive?
+        alive_cells_around_current_cell += 1 if cell_to_the_botton && cell_to_the_botton.alive?
+
+        if alive_cells_around_current_cell < 2
+          about_to_die << current_cell
+        end
+      end
+    end
+
+    about_to_die.each { |cell| cell.dead! }
   end
 end
